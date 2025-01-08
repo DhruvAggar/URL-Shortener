3197c7548a4e0cee1d062f5d59486502a3c8fae1
Dhruv Aggarwal <96526382+DhruvAggar@users.noreply.github.com>
2025-01-08 23:54:38 +0530

    Add files via upload

---
+++ models/url.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const urlSchema = new mongoose.Schema({
+    shortId: {
+        type: 'string',
+        required: true,
+        unique: true
+    },
+    redirectURL: {
+        type: 'string',
+        required: true
+    },
+    visitHistory: [{ timestamp: { type: Number}}]
+}, {timestamps: true}
+);
+
+
+const Url = mongoose.model('Url', urlSchema);
+
+module.exports = Url;
